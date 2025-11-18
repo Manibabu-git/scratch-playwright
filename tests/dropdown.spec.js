f79d037b07cdd550dbbf82c07e18f5fe256b8c34
Manibabu <manibabu.uppu1@gmail.com>
2025-11-18 08:45:05 +0530

    second commit

--- tests/dropdown.spec.js
+++ tests/dropdown.spec.js
@@ -1,15 +1,16 @@
 const { test, expect } = require("@playwright/test");
 
-// test("dropdown validation", async ({ page }) => {
-//   await page.goto("https://testautomationpractice.blogspot.com/");
-//   await page.locator("#male").click();
-//   await page.waitForTimeout(5000);
-// });
+test("dropdown validation", async ({ page }) => {
+  await page.goto("https://testautomationpractice.blogspot.com/");
+  await page.locator("#male").click();
+  await page.waitForTimeout(5000);
+});
 test("checkboxes", async ({ page }) => {
   await page.goto("https://testautomationpractice.blogspot.com/");
   const days = await page.$$(".form-group input[type='checkbox']");
   for (const day of days) {
-    if (day.textContent() == "sunday") await day.click();
+    if (day.textContent() == "sunday" || day.textContent() == "tuesday")
+      await day.click();
   }
   await page.waitForTimeout(5000);
 });
